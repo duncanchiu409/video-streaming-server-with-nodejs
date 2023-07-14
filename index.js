d5788a3f50bee8f4a2df827b21a3647a32ae8685
Tsz Wang Chiu <tszwangchiu@Tszs-MacBook-Air.local>
2023-07-14 17:24:14 +0930

    [checkpoint] success return 400 for non range headers

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
+app.get('/video', (req, res) => {
+    const range = req.headers.range
+    if(range === undefined){
+        res.status(400).send('Require range in headers')
+    }
+})
+
 app.listen(8000, () => {
     console.log('Listening on port 8000')
 })
