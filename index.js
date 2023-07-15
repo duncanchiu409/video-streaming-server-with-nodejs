d6300a219f01b63a4a83a4a4788f65a78fabd592
Tsz Wang Chiu <tszwangchiu@Tszs-MacBook-Air.local>
2023-07-15 16:22:06 +0930

    [checkpoint] video streaming over tcp/ip with pipe method
    However, I still do not understand how pipe method works compare to res.send().

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 const path = require('path')
 const fs = require('fs')
 
+const CHUNK_SIZE = 10 ** 6
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
@@ -14,8 +16,24 @@ app.get('/video', (req, res) => {
         res.status(400).send('Require range in headers')
     }
     else{
-        const videoSize = fs.statSync('pexels_videos_2282013 (2160p).mp4').size
-        res.send('Video size: ' + videoSize.toString())
+        const videoPath = 'pexels_videos_2282013 (2160p).mp4'
+        const videoSize = fs.statSync(videoPath).size
+
+        const start = Number(range.replace(/\D/g, ''))
+        const end = Math.min(start + CHUNK_SIZE , videoSize - 1)
+
+        const contentLength = end - start + 1
+
+        const headers = {
+            'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': contentLength,
+            'Content-Type': 'video/mp4'
+        }
+
+        res.writeHead(206, headers)
+        const videoStream = fs.createReadStream(videoPath, {start:start, end:end})
+        videoStream.pipe(res)
     }
 })
 
